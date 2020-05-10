2f9d8aafb7eeac428945414d02b93e512f477427
nhuhung19 <nhuhung19@gmail.com>
2020-05-10 22:43:47 +0700

    add heroku again

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,5 @@
 import socketIOClient from "socket.io-client";
 // const socket = socketIOClient("http://localhost:5000");
-const socket = socketIOClient("http://chatlog-back-end.herokuapp.com/");
+const socket = socketIOClient("https://chatlog-back-end.herokuapp.com/");
 
 export default socket
